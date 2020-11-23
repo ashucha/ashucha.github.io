0e1dc2abd882ea9d603f825e37ea2dd2478e6510
Ashu Cha <ashucha@github.com>
2020-11-22 21:03:27 -0500

    Improved nav UX and added picture

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ if (localStorage.getItem("page") && localStorage.getItem("page") !== "about") {
   $("#about").addClass("d-none");
 }
 
+if (localStorage.getItem("link") && localStorage.getItem("link") !== "about") {
+  $(".nav-link").removeClass("active");
+  console.log($(`#show-${localStorage.getItem("link")}`).html());
+  $(`#show-${localStorage.getItem("link")}`).addClass("active");
+}
+
 const showAbout = () => {
   window.scrollTo({ top: 0 });
   localStorage.setItem("page", "about");
+  localStorage.setItem("link", "about");
   $("#about").removeClass("d-none");
   $("#experience").addClass("d-none");
   $("#skills").addClass("d-none");
@@ -17,6 +24,7 @@ const showAbout = () => {
 const showExperience = () => {
   window.scrollTo({ top: 0 });
   localStorage.setItem("page", "experience");
+  localStorage.setItem("link", "experience");
   $("#experience").removeClass("d-none");
   $("#social-btn-group").removeClass("d-none");
   $("#about").addClass("d-none");
@@ -27,6 +35,7 @@ const showExperience = () => {
 const showSkills = () => {
   window.scrollTo({ top: 0 });
   localStorage.setItem("page", "skills");
+  localStorage.setItem("link", "skills");
   $("#social-btn-group").removeClass("d-none");
   // $("#experience").removeClass("d-none");
   // $("#about").addClass("d-none");
@@ -36,6 +45,7 @@ const showSkills = () => {
 const showAwards = () => {
   window.scrollTo({ top: 0 });
   localStorage.setItem("page", "awards");
+  localStorage.setItem("link", "awards");
   $("#awards").removeClass("d-none");
   $("#social-btn-group").removeClass("d-none");
   $("#about").addClass("d-none");
