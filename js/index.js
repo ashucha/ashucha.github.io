0947a749f6a76c4f75614b840d2d09a12176261a
achakragiri6 <ashucha03@gmail.com>
2025-06-11 00:32:01 -0700

    Add mobile viewing recommendation modal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(() => {
 	$("head").load("./templates/head.html");
 	$("#navbar-placeholder").load("./templates/navbar.html");
+
+	// Hide mobile modal on "Continue" button click
+	$("#continue-btn").on("click", function () {
+		$("#mobile-overlay-modal").hide();
+	});
 });
